29f9b5f7561f35742d68807f5095819de36cac85
ITLAB <ITLAB@IT>
2017-02-07 14:26:28 +0530

    second commit

--- src/com/example/lab3/MainActivity.java
+++ src/com/example/lab3/MainActivity.java
@@ -1,35 +1,70 @@
 package com.example.lab3;
 
+ 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
-
-
+import android.view.View;
+ 
 public class MainActivity extends Activity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+ 
+	DemoView demo;
+ 
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		demo = new DemoView(this);
+		setContentView(demo);
+	}
+ 
+	private class DemoView extends View {
+		public DemoView(Context mcontext) {
+			super(mcontext);
+		}
+ 
+		@Override
+		protected void onDraw(Canvas canvas) {
+			super.onDraw(canvas);
+ 
+			Paint paint = new Paint();
+			paint.setStyle(Paint.Style.FILL);
+ 
+			
+			paint.setColor(Color.BLUE);
+			canvas.drawPaint(paint);
+ 
+			paint.setAntiAlias(false);
+			paint.setColor(Color.MAGENTA);
+			canvas.drawCircle(150, 150, 40, paint);
+ 
+			paint.setAntiAlias(true);
+			paint.setColor(Color.CYAN);
+			canvas.drawCircle(150, 300, 60, paint);
+ 
+			
+			paint.setAntiAlias(false);
+			paint.setColor(Color.YELLOW);
+			canvas.drawRect(100, 15, 300, 70, paint);
+			
+			canvas.rotate(-25);
+ 
+			paint.setStyle(Paint.Style.FILL);
+			canvas.drawText("Graphics Rotation", 80, 280, paint);
+ 
+		
+			canvas.restore();
+		}
+	}
+ 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+ 
 }
